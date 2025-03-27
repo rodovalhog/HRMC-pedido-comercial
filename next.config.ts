61e9fb202edd84362dc03cf584e124499156394a
Guilherme Rodovalho <rodovalhogdeveloper@gmail.com>
2025-03-27 17:33:15 -0300

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+
+
+// next.config.js
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: false, // desativa minificação do SWC
+  // ou:
+  // minify: false, // se estiver usando o Terser (Next < 12)
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
