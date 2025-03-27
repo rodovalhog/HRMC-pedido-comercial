dcf7cd9da613b14efbf13f8ae7ab75f3ddd3bdff
Guilherme Rodovalho <rodovalhogdeveloper@gmail.com>
2025-03-27 17:35:30 -0300

    assinatura com clique

--- src/app/component/Assinatura.tsx
+++ src/app/component/Assinatura.tsx
@@ -1,28 +1,65 @@
-'use client';
+// 'use client';
+// import React, { useRef } from "react";
+// import SignatureCanvas from "react-signature-canvas";
+
+// const AssinaturaDigital = ({
+//   title,
+//   setValue,
+//   name
+// }: {
+//   title: string;
+//   setValue: any
+//   name: string;
+// }) => {
+//   const sigRef = useRef<SignatureCanvas>(null);
+
+//   const limparAssinatura = () => {
+//     sigRef.current?.clear();
+//     setValue(name, "");
+//   };
+
+//   const capturarAssinatura = () => {
+//     const imagemBase64 = sigRef.current?.getTrimmedCanvas().toDataURL("image/png");
+//     if (imagemBase64) {
+//       setValue(name, imagemBase64);
+//     }
+//   };
+
+//   return (
+//     <div className="flex flex-col items-center gap-2">
+//       <h2>{title}</h2>
+//       <SignatureCanvas
+//         ref={sigRef}
+//         penColor="black"
+//         onEnd={capturarAssinatura}
+//         canvasProps={{ className: "border rounded-lg w-full md:w-[500px] h-[150px]" }}
+//       />
+//       <div className="flex gap-4 mt-2">
+//         <button type="button" onClick={limparAssinatura}>Limpar</button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AssinaturaDigital;
+
+'use client'
+// components/AssinaturaDigital.tsx
 import React, { useRef } from "react";
 import SignatureCanvas from "react-signature-canvas";
 
-const AssinaturaDigital = ({
-  title,
-  setValue,
-  name
-}: {
-  title: string;
-  setValue: any
-  name: string;
-}) => {
+const AssinaturaDigital = ({ title, setValue, name }: { title: string, setValue: (e: any, x: any) => void, name?: string }) => {
   const sigRef = useRef<SignatureCanvas>(null);
 
   const limparAssinatura = () => {
     sigRef.current?.clear();
-    setValue(name, "");
   };
 
-  const capturarAssinatura = () => {
+  const salvarAssinatura = () => {
     const imagemBase64 = sigRef.current?.getTrimmedCanvas().toDataURL("image/png");
-    if (imagemBase64) {
-      setValue(name, imagemBase64);
-    }
+    console.log("image base", imagemBase64);
+    setValue(name, imagemBase64,)
+    // Você pode salvar no backend ou enviar para API
   };
 
   return (
@@ -31,11 +68,11 @@ const AssinaturaDigital = ({
       <SignatureCanvas
         ref={sigRef}
         penColor="black"
-        onEnd={capturarAssinatura}
         canvasProps={{ className: "border rounded-lg w-full md:w-[500px] h-[150px]" }}
       />
       <div className="flex gap-4 mt-2">
         <button type="button" onClick={limparAssinatura}>Limpar</button>
+        <button type="button" onClick={salvarAssinatura}>Salvar</button>
       </div>
     </div>
   );
