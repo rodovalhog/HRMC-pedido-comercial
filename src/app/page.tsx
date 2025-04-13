d6bc9677e6c2471e30af7fef5a8ead715c893733
Guilherme Rodovalho <rodovalhogdeveloper@gmail.com>
2025-04-13 15:21:49 -0300

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
 }
 
 
-const WrapperStep = ({ children, contentHeader, contentFooter }: { children: ReactNode, contentHeader?: any, contentFooter?: any }) => {
+const WrapperStep = ({ children, contentHeader }: { children: ReactNode, contentHeader?: any, contentFooter?: any }) => {
   return (<div className='flex flex-col min-h-screen md:container mx-auto relative'>
     {!contentHeader && (<header className="bg-blue-700  w-full h-16  shadow ">
       <div className='max-w-7xl mx-auto px-4 flex items-center h-full font-bold text-2xl tracking-wide flex justify-center'>
@@ -23,11 +23,11 @@ const WrapperStep = ({ children, contentHeader, contentFooter }: { children: Rea
     <main >
       {children}
     </main>
-    {!contentFooter && (<footer className="bg-blue-700  absolute bottom-0  w-full h-16  shadow z-50 h-[100px]">
+    {/* {!contentFooter && (<footer className="bg-blue-700  absolute bottom-0  w-full h-16  shadow z-50 h-[100px]">
       <div className='max-w-7xl mx-auto px-4 flex items-center h-full font-bold text-2xl tracking-wide'>
         Resumo do pedido: R$: 2000,00
       </div>
-    </footer>)}
+    </footer>)} */}
 
   </div>)
 }
