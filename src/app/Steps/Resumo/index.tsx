99458bc8d26e9c2217c590858d5ba851363ce98a
Guilherme Rodovalho <rodovalhogdeveloper@gmail.com>
2025-04-13 15:51:55 -0300

    ok

--- src/app/Steps/Resumo/index.tsx
+++ src/app/Steps/Resumo/index.tsx
@@ -15,19 +15,19 @@ const ResumoPedido = () => {
   const total = totalPares * produto.valor;
 
   return (
-    <div className="max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-xl mt-10 text-gray-500">
+    <div className=" mx-auto p-6 bg-white shadow-lg rounded-xl mt-10 text-gray-500">
       <h1 className="text-2xl font-bold mb-4">Resumo do Pedido</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
         <div>
           <h2 className="text-lg font-semibold">Dados do Cliente</h2>
-          <p><strong>Razão Social:</strong> {dados.razaoSocial}</p>
-          <p><strong>CNPJ:</strong> {dados.cnpj}</p>
-          <p><strong>Inscrição Estadual:</strong> {dados.inscricaoEstadual}</p>
-          <p><strong>Endereço:</strong> {dados.endereco}</p>
-          <p><strong>Cidade:</strong> {dados.municipio} - {dados.uf}</p>
-          <p><strong>Email:</strong> {dados.email}</p>
-          <p><strong>Telefone:</strong> {dados.telefone}</p>
+          <p><strong>Razão Social:</strong> {dados?.razaoSocial}</p>
+          <p><strong>CNPJ:</strong> {dados?.cnpj}</p>
+          <p><strong>Inscrição Estadual:</strong> {dados?.inscricaoEstadual}</p>
+          <p><strong>Endereço:</strong> {dados?.endereco}</p>
+          <p><strong>Cidade:</strong> {dados?.municipio} - {dados?.uf}</p>
+          <p><strong>Email:</strong> {dados?.email}</p>
+          <p><strong>Telefone:</strong> {dados?.telefone}</p>
         </div>
 
         <div>
@@ -44,20 +44,25 @@ const ResumoPedido = () => {
       <div className="mb-4">
         <h2 className="text-lg font-semibold">Grade de Tamanhos</h2>
         <div className="flex flex-wrap gap-4 mt-2">
-          {Object.entries(produto.quantidades).map(([tamanho, qtd]: any) => (
-            <div key={tamanho} className="border p-2 rounded shadow text-center w-16">
-              <p className="font-semibold text-sm"> {tamanho}</p>
-              <div>{qtd}</div>
-            </div>
-          ))}
+          {Object.entries(produto.quantidades).map(([tamanho, qtd]: any) => {
+
+            if (Number.isNaN(qtd)) return
+            return (
+              <div key={tamanho} className="border p-2 rounded shadow text-center w-16">
+                <p className="font-semibold text-sm"> {tamanho}</p>
+                <div>{qtd}</div>
+              </div>
+            )
+
+          })}
         </div>
       </div>
 
       <div className="mb-4">
         <h2 className="text-lg font-semibold">Pagamento</h2>
-        <p><strong>Tipo de Pagamento:</strong> {dados.paymentTypes.join(', ')}</p>
-        <p><strong>Forma:</strong> {dados.avistaType}</p>
-        <p><strong>Observações:</strong> {dados.observations}</p>
+        <p><strong>Tipo de Pagamento:</strong> {dados?.paymentTypes?.join(', ')}</p>
+        <p><strong>Forma:</strong> {dados?.avistaType}</p>
+        <p><strong>Observações:</strong> {dados?.observations}</p>
       </div>
     </div>
   );
