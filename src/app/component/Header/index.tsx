dca099ebe96b27fee518bb1e5bac24539655d9eb
Guilherme Rodovalho <rodovalhogdeveloper@gmail.com>
2025-04-26 14:16:31 -0300

    gerar pedido pdf

--- src/app/component/Header/index.tsx
+++ src/app/component/Header/index.tsx
@@ -2,8 +2,8 @@
 
 export const Header = () => {
   return (
-    <header className="fixed top-0 left-0 w-full bg-blue-500 shadow z-50 h-16 flex items-center px-6 ">
-      <h1 className="text-xl font-bold">HRMC Representações</h1>
+    <header className="fixed flex justify-center top-0 left-0 w-full bg-blue-500 shadow z-50 h-16 flex items-center px-6 ">
+      <h1 className="text-xl font-bold text-center">HRMC Representações</h1>
     </header>
   );
 };
