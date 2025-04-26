c1df8d26e6def9ed40ffeda7df2529a32562aac3
Guilherme Rodovalho <rodovalhogdeveloper@gmail.com>
2025-04-26 14:14:11 -0300

    gerar pedido pdf

--- src/app/component/Header/index.tsx
+++ src/app/component/Header/index.tsx
@@ -3,7 +3,7 @@
 export const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full bg-blue-500 shadow z-50 h-16 flex items-center px-6 ">
-      <h1 className="text-xl font-bold">Meu Site</h1>
+      <h1 className="text-xl font-bold">HRMC Representações</h1>
     </header>
   );
 };
